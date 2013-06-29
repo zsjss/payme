f07ab5ce9f14f216e45a7ef1aa77306d9a03f605
gtt116 <gtt116@gmail.com>
2013-06-29 22:29:59 +0800

    implement sign in logic

--- portal/views/base.py
+++ portal/views/base.py
@@ -6,36 +6,55 @@ For example: signin, signup, homepage , etc.
 """
 
 import functools
+import logging
 
 #from django import http
 from . import render
 from portal import forms
 from portal import models
 
+LOG = logging.getLogger(__name__)
+
 
 def check_authentication(func):
-    """The decorator will check username and password from session.
-    If the username and password is correct contiune.. otherwise redirect
-    to sign_in page.
+    """The decorator will check username existed in session. If it is continue.
     """
     @functools.wraps(func)
     def _check_authentication(req):
         username = req.session.get('username')
-        password = req.session.get('password')
-        if username and password:
-            user = models.User.object.filter(username=username)
-            if password == user.password:
-                # everything is ok, continue.
-                func(req)
-        #otherwise, redirect to signin page.
-        return signin(req)
+        if not username:
+            return signin(req)
+        else:
+            func(req)
 
     return _check_authentication
 
 
+def _set_session(req, username):
+    req.session['username'] = username
+
+
 def signin(req):
     """登陆"""
-    return render('sign_in.html', {})
+    if req.method == 'GET':
+        form = forms.SignInForm()
+        return render('sign_in.html', {'form': form})
+    elif req.method == 'POST':
+        form = forms.SignInForm(req.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = models.User.objects.filter(username=data['username'])[0]
+            if user.password == data['password']:
+                LOG.debug('%s login success.' % user)
+                _set_session(req, user.username)
+                return portal(req)
+            else:
+                LOG.debug("%s login failed." % user)
+                return render('sign_in.html',
+                                {'errors': 'Username or password wrong',
+                                 'form': form})
+        else:
+            return render('sign_in.html', {'form': form})
 
 
 def signup(req):
@@ -54,7 +73,6 @@ def signup(req):
             return render('sign_up.html', {'form': form})
 
 
-@check_authentication
 def portal(req):
     return render('portal.html', {})
 
