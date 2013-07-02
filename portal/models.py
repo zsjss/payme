194597c646d55bd6fb85f0ac369eed0af4a4b3c1
gtt116 <gtt116@gmail.com>
2013-07-02 22:02:49 +0800

    Change database models.
    LandlordRentProfile -- 收房租第一表单
    LandlordRenterInfo  -- 收房租第二表单（增加租客）
    RenterRentProfile  -- 付房租第一表单
    RenterOption       -- 付房租第二表单

--- portal/models.py
+++ portal/models.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Database models are defined here.
-
-Currently relationship is shown below:
-
-User:1 ---- House:*
-House:1 ---- Order:*
 """
 
-from django.db import models
+from django.db.models import Model
+from django.db.models import IntegerField, CharField, DateTimeField
+from django.db.models import DateField, EmailField, BooleanField
+from django.db.models import ForeignKey, OneToOneField
 from django.contrib import admin
 
 
 ################################
-class User(models.Model):
-    username = models.CharField(max_length=255, db_index=True, unique=True)
-    password = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20, db_index=True)
-    real_name = models.CharField(max_length=255)
-    real_id = models.CharField(max_length=40)
-    email = models.EmailField()
-    signup_at = models.DateTimeField(auto_now=True)
-    avater = models.CharField(max_length=255)
-    location_province = models.CharField(max_length=255)
-    location_city = models.CharField(max_length=255)
+class User(Model):
+    username = CharField(max_length=255, db_index=True, unique=True)
+    password = CharField(max_length=255)
+    phone = CharField(max_length=20, db_index=True)
+    real_name = CharField(max_length=255)
+    real_id = CharField(max_length=40)
+    email = EmailField()
+    signup_at = DateTimeField(auto_now=True)
+    avater = CharField(max_length=255)
+    location_province = CharField(max_length=255)
+    location_city = CharField(max_length=255)
 
     def __unicode__(self):
         return self.username
@@ -34,42 +32,117 @@ class UserAdmin(admin.ModelAdmin):
 
 
 ##################################
-class House(models.Model):
-    owner = models.ForeignKey(User)
-    house_name = models.CharField(max_length=255)
-    house_size = models.IntegerField()
-    house_style = models.CharField(max_length=30)
+class LandlordRentProfile(Model):
+    """A user who is landlord can have many rent."""
+    landlord = ForeignKey(User)
 
-    def __unicode__(self):
-        return self.house_name
+    rent_type = CharField(max_length=2,
+                                 choices=[(1, 'house'), (2, 'shop')])
+    room_count = CharField(max_length=2,
+                                  choices=[(1, '1'), (2, '2'),
+                                          (3, '3'), (4, '>=4')])
+    deck = CharField(max_length=2)
+    acreage = IntegerField()
+    loc_province = CharField(max_length=50)
+    loc_city = CharField(max_length=50)
+    loc_area = CharField(max_length=50)
+    loc_cell = CharField(max_length=50)
+    loc_addr = CharField(max_length=50)
 
-
-class HouseAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'house_name']
-
-
-##################################
-class Order(models.Model):
-    house = models.ForeignKey(House)
-    renter = models.ForeignKey(User)
-
-    def __unicode__(self):
-        return self.id
-
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['house', 'renter']
-
-
-###################################
-class Payment(models.Model):
-    to_who = models.ForeignKey(User, related_name='payment_to_me_set')
-    from_who = models.ForeignKey(User, related_name='payment_from_me_set')
-    money = models.IntegerField()
+    payee_name = CharField(max_length=255)
+    payee_id_card = CharField(max_length=20)
+    payee_bank = CharField(max_length=255)
+    payee_bank_card = CharField(max_length=50)
+    payee_bank_province = CharField(max_length=50)
+    payee_bank_city = CharField(max_length=50)
 
     def __unicode__(self):
         return self.id
 
 
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['money', 'to_who', 'from_who']
+class LandlordRenterInfo(Model):
+    """A Landlord can have many renter. Each renter pay difference expense."""
+    rent = ForeignKey(LandlordRentProfile)
+    renter = ForeignKey(User)
+
+    renter_name = CharField(max_length=255)
+    rent_expense = IntegerField()
+    rent_monthsn = IntegerField()
+    rent_begin_date = DateField()
+    rent_pay_model = CharField(max_length=2, choices=[
+        (1, 'one month'), (2, 'three month'),
+        (4, 'six month'), (8, 'twenty month')])
+    pay_months = IntegerField()
+    pay_begin_date = DateField()
+    deposit = IntegerField()
+    arrive_type = CharField(max_length=2, choices=[
+        (1, 'next day'), (2, 'two hours')])
+    i_pay_it = BooleanField()
+
+    service_expense = IntegerField()
+    total_expense = IntegerField()
+    created_at = DateTimeField(auto_now=True)
+
+    state = CharField(max_length=2, choices=[(1, 1), (2, 2), (3, 3),
+                                             (4, 4), (5, 5)])
+
+    def __unicode__(self):
+        return self.id
+
+
+class RenterRentProfile(Model):
+    """A renter can rent many house."""
+    renter = ForeignKey(User)
+
+    rent_type = CharField(max_length=2,
+                                 choices=[(1, 'house'), (2, 'shop')])
+    room_count = CharField(max_length=2,
+                                  choices=[(1, '1'), (2, '2'),
+                                          (3, '3'), (4, '>=4')])
+    deck = CharField(max_length=2)
+    acreage = IntegerField()
+
+    rent_expense = IntegerField()
+    rent_months = IntegerField()
+    rent_begin_date = DateField()
+
+    loc_province = CharField(max_length=50)
+    loc_city = CharField(max_length=50)
+    loc_area = CharField(max_length=50)
+    loc_cell = CharField(max_length=50)
+    loc_addr = CharField(max_length=50)
+
+    payee_type = CharField(max_length=2, choices=[
+        (1, 'person'), (2, 'company')])
+    payee_name = CharField(max_length=255)
+    payee_bank = CharField(max_length=255)
+    payee_bank_card = CharField(max_length=50)
+    payee_bank_province = CharField(max_length=50)
+    payee_bank_city = CharField(max_length=50)
+    payee_phone = CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.id
+
+
+class RenterOption(Model):
+    """A RenterRentProfile can only have one RenterOption"""
+    rent_profile = OneToOneField(RenterRentProfile)
+
+    pay_months = IntegerField()
+    pay_begin_data = DateField()
+    total_expense = IntegerField()
+    deposit = IntegerField()
+    comment = CharField(max_length=255)
+    arrive_type = CharField(max_length=2, choices=[
+        (1, 'next day'), (2, 'two hours')])
+    is_installment = BooleanField()
+
+    service_expense = IntegerField()
+    created_at = DateTimeField(auto_now=True)
+
+    state = CharField(max_length=2, choices=[(1, 1), (2, 2), (3, 3),
+                                             (4, 4), (5, 5)])
+
+    def __unicode__(self):
+        return self.id
