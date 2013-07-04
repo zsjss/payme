d67101903fa724bf4c943f106b610285c5caac23
gtt116 <gtt116@gmail.com>
2013-07-05 02:08:53 +0800

    Implement basic create rent, update rent database logic.

--- portal/views/charge_rent.py
+++ portal/views/charge_rent.py
@@ -16,7 +16,10 @@ class ChargeRentCreateView(generic.FormView):
     template_name = 'portal/charge/create.html'
 
     def form_valid(self, form):
-        return redirect('charge_renter_add', 'hehe')
+        user = utils.get_user_obj(self.request)
+        form.instance.landlord = user
+        form.save()
+        return redirect('charge_renter_add', form.instance.pk)
 
 charge_rent_create = require_auth(ChargeRentCreateView.as_view())
 
@@ -33,7 +36,13 @@ class ChargeRentUpdateView(generic.FormView):
         return x.as_dict()
 
     def form_valid(self, form):
-        pass
+        rent_id = self.args[0]
+        assert rent_id
+        x = get_object_or_404(models.LandlordRentProfile, pk=rent_id)
+        x.update_from_dict(form.cleaned_data)
+        x.save()
+        return redirect('portal')
+
 charge_rent_update = require_auth(ChargeRentUpdateView.as_view())
 
 
@@ -45,16 +54,19 @@ class ChargeRentCreateView(generic.FormView):
     def form_valid(self, form):
         rent_id = self.args[0]
         assert rent_id
+        #TODO: save renter info into databse.
         return redirect('charge_renter_confirm', 'hehe', 'haha')
 
 charge_renter_add = require_auth(ChargeRentCreateView.as_view())
 
 
 def charge_renter_confirm(request, profile_id, renter_info_id):
+    #TODO:
     return utils.render('charge/renter_confirm.html',
                         {'rent_id': profile_id,
                          'renter_id': renter_info_id})
 
 
 def charge_renter_done(request, profile_id, renter_info_id):
+    #TODO:
     return utils.render('charge/renter_finish.html', {})
