3568e6c7e3b1615a7f7f06bdaa6ef633b4fb2175
周诗杰 <sjzhouss@gmail.com>
2013-07-07 10:39:03 +0800

    zsj_message_function

--- portal/views/account.py
+++ portal/views/account.py
@@ -5,6 +5,8 @@ The account views are here.
 
 from portal.views import utils
 from portal.views.base import require_auth
+from portal.models import User
+from portal.models import Message, BankCard
 
 
 @require_auth
@@ -34,7 +36,11 @@ def safe(request):
 
 @require_auth
 def messges(request):
-    pass
+    username = request.session.get('username')
+    user = User.objects.filter(username = username)
+    user_id = user[0].id
+    message = Message.objects.filter(owner_id = user_id)
+    return utils.render('account_messages.html',{'message': message})
 
 
 @require_auth
