612d9bc3eadd046dc5d50f914bb70e3a1bb115d2
gtt116 <gtt116@gmail.com>
2013-07-07 16:55:42 +0800

    Add base url tests.
    Also change to url setting to match tests requirement.

--- portal/tests.py
+++ portal/tests.py
@@ -10,9 +10,39 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+URL_PREFIX = '/z/'
+
+
+class UrlTestCase(TestCase):
+
+    def test_200_url(self):
+        """Make sure all url works."""
+        urls = [
+            '/',
+            'signin/',
+            'signup/',
+            'logout/',
+            'chargerent/',
+            'chargerent/1/',
+            'chargerent/1/update/',
+            'chargerent/1/addrenter/',
+            'chargerent/1/addrenter/1/confirm/',
+            'chargerent/1/addrenter/1/done/',
+            'chargerent/1/renter/1/cancel/',
+            'account/',
+            'account/payrentlist/',
+            'account/chargerentlist/',
+            'account/payments/',
+            'account/safe/',
+            'account/info/',
+            'account/cardmanage/',
+            'account/vip/',
+        ]
+
+        for url in urls:
+            url = URL_PREFIX + url
+            resp = self.client.get(url, follow=True)
+            self.assertEqual(200, resp.status_code,
+                             '%s != %s, path: %s, %s' %
+                             (200, resp.status_code, url, resp.content))
+
