586b19fdf018732c7fe2d36e79b2a4e6f2c6e315
smartbrandnew <smartbrandnew@163.com>
2013-07-21 09:44:57 +0800

    new file

--- portal/views/account.py
+++ portal/views/account.py
@@ -54,8 +54,8 @@ def info(req):
     user = models.User.objects.filter(username=usernam)
     return utils.render('personal_info.html',
                         {'username': user[0].username,
-	                'phone': user[0].phone,
-			'real_id': user[0].real_id,
+                         'phone': user[0].phone,
+                         'real_id': user[0].real_id,
                          'real_name': user[0].real_name,
                          'email': user[0].email})	    
     
@@ -69,8 +69,8 @@ def safe(req):
     local_time = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(time.time()))
     return utils.render('security_certificate.html',
                         {'local_time': local_time,
-	                     'phone': user[0].phone})
-  
+                         'phone': user[0].phone})
+
 
 
 @check_authentication
@@ -84,16 +84,16 @@ class NameCertification(generic.FormView):
     template_name = 'portal/name_certificate.html'
 
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-	    if user:
-                user.real_name = data['real_name']
-		user.real_id = data['real_id']
-	        user.save()
-		return info(self.request)
-	    else:
-            #LOG.debug("%s name certificate failed." % user)
-                return utils.render('name_certificate.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        if user:
+            user.real_name = data['real_name']
+            user.real_id = data['real_id']
+            user.save()
+            return info(self.request)
+        else:
+        #LOG.debug("%s name certificate failed." % user)
+            return utils.render('name_certificate.html',
                                 {'errors': 'failed',
                                  'form': form})
 
@@ -106,15 +106,15 @@ class PasswordModify(generic.FormView):
     template_name = 'portal/password_modify.html'
 
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-	    if user and user.password == data['password'] and data['new_password'] == data['confirm_new_password']:
-                user.password = data['new_password']
-	        user.save()
-		return info(self.request)
-	    else:
-            #LOG.debug("%s password modify failed." % user)
-                return utils.render('password_modify.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        if user and user.password == data['password'] and data['new_password'] == data['confirm_new_password']:
+            user.password = data['new_password']
+            user.save()
+            return info(self.request)
+        else:
+        #LOG.debug("%s password modify failed." % user)
+            return utils.render('password_modify.html',
                                 {'errors': 'password is wrong',
                                  'form': form})
 
@@ -127,16 +127,16 @@ class PhoneModify(generic.FormView):
     template_name = 'portal/phone_modify.html'
        
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-	    print user.verifycode
-	    if user and data['phone'] and data['verification_code'] == str(user.verifycode):
-                user.phone = data['phone']
-	        user.save()
-		return safe(self.request)
-	    else:
-            #LOG.debug("%s phone modify failed." % user)
-                return utils.render('phone_modify.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        print user.verifycode
+        if user and data['phone'] and data['verification_code'] == str(user.verifycode):
+            user.phone = data['phone']
+            user.save()
+            return safe(self.request)
+        else:
+        #LOG.debug("%s phone modify failed." % user)
+            return utils.render('phone_modify.html',
                                 {'errors': 'phone number is wrong',
                                  'form': form})
 
@@ -149,17 +149,17 @@ class SendVerifyCode(generic.FormView):
     template_name = 'portal/send_verifycode.html'
        
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-            if user and data['phone']:
-		user.verifycode = random.randrange(0,999999)
-		user.save()
-		content = 'verifycode:' + str(user.verifycode)
-		utils.send_msg(data['phone'], content)
-		return phonemodify(self.request)
-	    else:
-            #LOG.debug("%s phone modify failed." % user)
-                return utils.render('send_verifycode.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        if user and data['phone']:
+            user.verifycode = random.randrange(0,999999)
+            user.save()
+            content = 'verifycode:' + str(user.verifycode)
+            utils.send_msg(data['phone'], content)
+            return phonemodify(self.request)
+        else:
+        #LOG.debug("%s phone modify failed." % user)
+            return utils.render('send_verifycode.html',
                                 {'errors': 'failed',
                                  'form': form})
     
@@ -172,17 +172,17 @@ class MailboxModify(generic.FormView):
     template_name = 'portal/mailbox_binding.html'
     
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-	    if user and data['email']:
-		content = 'click here to active ' + '<a href="http://www.zufangbao.com/chargerent/">http://www.active.com</a>'
-		utils.send_mail(data['email'], content)
-                user.email = data['email']
-	        user.save()
-		return safe(self.request)
-	    else:
-            #LOG.debug("%s mailbox modify failed." % user)
-                return utils.render('mailbox_modify.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        if user and data['email']:
+            content = 'click here to active ' + '<a href="http://www.zufangbao.com/chargerent/">http://www.active.com</a>'
+            utils.send_mail(data['email'], content)
+            user.email = data['email']
+            user.save()
+            return safe(self.request)
+        else:
+        #LOG.debug("%s mailbox modify failed." % user)
+            return utils.render('mailbox_modify.html',
                                 {'errors': 'failed',
                                  'form': form})
 
@@ -196,17 +196,17 @@ class SecurityProblem(generic.FormView):
     template_name = 'portal/security_problem.html'
     
     def form_valid(self, form):
-	    user = utils.get_user_obj(self.request)
-            data = form.cleaned_data
-	    if user and data['problem_one'] and data['problem_two'] and data['problem_three']:
-                user.problem_one = data['problem_one']
-		user.problem_two = data['problem_two']
-		user.problem_three = data['problem_three']
-	        user.save()
-		return safe(self.request)
-	    else:
-            #LOG.debug("%s failed." % user)
-                return utils.render('security_problem.html',
+        user = utils.get_user_obj(self.request)
+        data = form.cleaned_data
+        if user and data['problem_one'] and data['problem_two'] and data['problem_three']:
+            user.problem_one = data['problem_one']
+            user.problem_two = data['problem_two']
+            user.problem_three = data['problem_three']
+            user.save()
+            return safe(self.request)
+        else:
+        #LOG.debug("%s failed." % user)
+            return utils.render('security_problem.html',
                                 {'errors': 'failed',
                                  'form': form})
 
